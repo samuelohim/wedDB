ef0540e78f39d7084b77528f9bbb8528acc4c9b0
samuElohim <satlibwanzai@gmail.com>
2017-02-08 19:57:54 +0800

    main.blade.php, listusers.blade.php established, bootstrap nav bar implemented

--- app/Http/Controllers/syzKongZhiQi.php
+++ app/Http/Controllers/syzKongZhiQi.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\User;
 class syzKongZhiQi extends Controller
 {
     /**
@@ -14,6 +15,7 @@ class syzKongZhiQi extends Controller
     public function index()
     {
         //
+        return view('listusersb');
     }
 
     /**
