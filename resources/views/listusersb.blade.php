2d96b9d3b83110c4ddeb31b4fbbd2b9c58703af6
samuElohim <satlibwanzai@gmail.com>
2017-02-08 20:18:46 +0800

    delete, show, edit buttons implemented with corresponding colors

--- resources/views/listusersb.blade.php
+++ resources/views/listusersb.blade.php
@@ -26,15 +26,26 @@
 		      			<form action="/wedDB/{{$user->id}}" method = 'POST'>
 		      				{!! csrf_field() !!}
 		      				<input type="hidden" name = '_method' value = 'DELETE'>
-		      				<button type = 'submit'>
+		      				<button type = 'submit' class = 'btn btn-danger'>
 		      					<span class="glyphicon glyphicon-trash"></span>
 		      				</button>
 		      			</form>
 		      		</li>
-	      			<li></li>
-	      			<li></li>
-	      			<li></li>
-	      			<li></li>	
+	      			<li>
+		      			<form action="/wedDB/{{$user->id}}/edit" method = 'GET'>
+		      				{!! csrf_field() !!}
+		      				<button type = 'submit' class = 'btn btn-warning'>
+		      					<span class="glyphicon glyphicon-trash"></span>
+		      				</button>
+		      			</form></li>
+	      			<li>
+		      			<form action="/wedDB/{{$user->id}}" method = 'GET'>
+		      				{!! csrf_field() !!}
+		      				<button type = 'submit' class = 'btn btn-success'>
+		      					<span class="glyphicon glyphicon-trash"></span>
+		      				</button>
+		      			</form>
+		      		</li>	
 		      	</ul>
 		      </td>
 		    </tr>
